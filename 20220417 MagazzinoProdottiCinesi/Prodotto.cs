d44155cf134121e1a0ec55313b8856fdc6696c49
LukePC <LukePC@DESKTOP-SMI2KCQ>
2022-04-20 22:41:47 +0200

    ultimo

--- 20220417 MagazzinoProdottiCinesi/Prodotto.cs
+++ 20220417 MagazzinoProdottiCinesi/Prodotto.cs
@@ -6,28 +6,71 @@ using System.Threading.Tasks;
 
 namespace _20220417_MagazzinoProdottiCinesi
 {
-    public class Prodotto
+    public abstract class Prodotto
     {
         public string Nome { get; set; }
-        //public int quantita { get; set; }
+
         public int Prezzo { get; set; }
+        public int qt { get; set; }
+
+        public virtual void SpaccaProdotto(Magazzino magazzino)
+        {
+            Console.WriteLine("Ho spaccato il prodotto " + this.Nome);
+        }
 
     }
+
     public class Magazzino
     {
         public List<ProdottoInserito> prodottoInserito = new List<ProdottoInserito>();
-
+        public int qt { get; set; }
         public class ProdottoInserito
         {
-            public Prodotto prodotto;
-            public int Quantita;
+            private Prodotto prodotto;
+            public Prodotto Prodotto
+            {
+                get
+                {
+                    return prodotto;                    
+                }
+                set
+                {                   
+                    prodotto = value;
+                    //int qta = this.Quantita ;
+                    NumeroInserimenti.Inserimenti++; ;
+                }
+            }
+            public int Quantita;           
+        }
+        
+        public void VisualizzaProdotto()
+        {
+            bool haElementi = this.prodottoInserito.Any();
+            if (haElementi)
+            {
+                Console.WriteLine();
+                for (int i = 0; i < this.prodottoInserito.Count; i++)
+                {
+                    Console.WriteLine($"ID: {i} , Nome Prodotto: {this.prodottoInserito[i].Prodotto.Nome} Quantita': {this.prodottoInserito[i].Quantita}  Costo: {this.prodottoInserito[i].Prodotto.Prezzo}");
+                }
+                Console.WriteLine($"In totale sono stati fatti {NumeroInserimenti.Inserimenti} inserimenti");
+                Console.WriteLine("'Invio'");
+                this.qt = 0;
+                Console.ReadKey();
+            }
+            else
+            {
+                Console.WriteLine("Non ci sono prodotti inseriti.");
+                this.qt = 1;
+                Console.ReadKey();
+            }
         }
 
         public void AggiungiProdotto()
         {
             Console.WriteLine("Che prodotto vuoi inserire? 'Inserire la lettera corrispondente.'");
             Console.WriteLine("A) - Articoli per la casa");
-            Console.WriteLine("B) - Articoli per l'auto");            
+            Console.WriteLine("B) - Articoli per l'auto");
             string scelta = Console.ReadLine();
             if (scelta == "A")
             {
@@ -39,10 +82,11 @@ namespace _20220417_MagazzinoProdottiCinesi
                 Console.WriteLine("In quale stanza viene utilizzato?");
                 artcasa.StanzaUtilizzo = Console.ReadLine();
                 ProdottoInserito prodIns = new ProdottoInserito();
-                prodIns.prodotto = artcasa;
+                prodIns.Prodotto = artcasa;
                 Console.WriteLine("Inserisi la quantità");
                 prodIns.Quantita = int.Parse(Console.ReadLine());
-                this.prodottoInserito.Add(prodIns);              
+                this.prodottoInserito.Add(prodIns);
+
             }
             else if (scelta == "B")
             {
@@ -54,26 +98,108 @@ namespace _20220417_MagazzinoProdottiCinesi
                 Console.WriteLine("Quale parte dell'auto ripara?");
                 artAuto.CosaRipara = Console.ReadLine();
                 ProdottoInserito prodIns = new ProdottoInserito();
-                prodIns.prodotto = artAuto;
+                prodIns.Prodotto = artAuto;
                 Console.WriteLine("Inserisi la quantità");
                 prodIns.Quantita = int.Parse(Console.ReadLine());
                 this.prodottoInserito.Add(prodIns);
-            }            
+            }
         }
+        public void AggiungiStock()
+        {
+            ArtCasa artcasa1 = new ArtCasa();
+            artcasa1.Nome = "Sapone";
+            artcasa1.Prezzo = 3;
+            artcasa1.StanzaUtilizzo = "Salotto";
+            ProdottoInserito prodIns1 = new ProdottoInserito();
+            prodIns1.Prodotto = artcasa1;
+            prodIns1.Quantita = 50;
+            this.prodottoInserito.Add(prodIns1);
+
+            ArtCasa artcasa2 = new ArtCasa();
+            artcasa2.Nome = "Forchetta";
+            artcasa2.Prezzo = 1;
+            artcasa2.StanzaUtilizzo = "Cantina";
+            ProdottoInserito prodIns2 = new ProdottoInserito();
+            prodIns2.Prodotto = artcasa2;
+            prodIns2.Quantita = 10;
+            this.prodottoInserito.Add(prodIns2);
+
+            ArtAuto artAuto1 = new ArtAuto();
+            artAuto1.Nome = "Cacciavite";
+            artAuto1.Prezzo = 15;
+            artAuto1.CosaRipara = "Pneumatici";
+            ProdottoInserito prodIns3 = new ProdottoInserito();
+            prodIns3.Prodotto = artAuto1;
+            prodIns3.Quantita = 20;
+            this.prodottoInserito.Add(prodIns3);
+
+            ArtAuto artAuto2 = new ArtAuto();
+            artAuto2.Nome = "Martello";
+            artAuto2.Prezzo = 20;
+            artAuto2.CosaRipara = "Parabrezza";
+            ProdottoInserito prodIns4 = new ProdottoInserito();
+            prodIns4.Prodotto = artAuto2;
+            prodIns4.Quantita = 30;
+            this.prodottoInserito.Add(prodIns4);
+        }
+        public void EliminaProdotto()
+        {
+            VisualizzaProdotto();
+            if (this.qt == 1)
+            {
+
+            }
+            else
+            {
+                Console.WriteLine("Inserisci il numero ID del prodotto che vuoi eliminare");
+                int sceltaNum = int.Parse(Console.ReadLine());
+                Console.WriteLine("Inserisci la quantità di pezzi che vuoi portare via");
+                int qtaElim = int.Parse(Console.ReadLine());
+                if (this.prodottoInserito[sceltaNum].Quantita < qtaElim)
+                {
+                    Console.WriteLine("Mi dispiace non puoi prenderne, non ce ne sono abbastanza.");
+                }
+                else
+                {
+                    this.prodottoInserito[sceltaNum].Quantita -= qtaElim;
+                }
+                Console.WriteLine($"ID: {sceltaNum} , Nome Prodotto: {this.prodottoInserito[sceltaNum].Prodotto.Nome} Quantita': {this.prodottoInserito[sceltaNum].Quantita}  Costo: {this.prodottoInserito[sceltaNum].Prodotto.Prezzo}");
+                Console.ReadKey();
+            }
 
 
 
-
+        }
     }
     public class ArtCasa : Prodotto
     {
         public string StanzaUtilizzo { get; set; }
+
+        public override void SpaccaProdotto(Magazzino magazzino)
+        {
+            base.SpaccaProdotto(magazzino);
+            Console.WriteLine("Lanciandolo contro la casa");
+            //Console.ReadKey();
+
+        }
     }
     public class ArtAuto : Prodotto
     {
         public string CosaRipara { get; set; }
-    }
 
+        public override void SpaccaProdotto(Magazzino magazzino)
+        {
+            base.SpaccaProdotto(magazzino);
+            Console.WriteLine("Lanciandolo contro l'auto");
+            //Console.WriteLine();
+
+        }
+    }
+    public class NumeroInserimenti
+    {
+        public static int Inserimenti { get; set; }
+
+    }
 
 
 
--- 20220417 MagazzinoProdottiCinesi/Program.cs
+++ 20220417 MagazzinoProdottiCinesi/Program.cs
@@ -4,12 +4,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-// oggetti statici e dinamici
-// ereditarietà
-// Add List
-// polimorfismo
-// interfacce
-// Altro?
+// oggetti statici e dinamici   -->  ?
+// ereditarietà                 --> OK
+// Add List                     --> OK
+// polimorfismo                 --> OK Spacco
+// interfacce                   -->  ?
+// abstract                     -->  ok Prodotto Abstract
+// Altro?                       -->  ?
+
 
 
 namespace _20220417_MagazzinoProdottiCinesi
@@ -18,6 +20,64 @@ namespace _20220417_MagazzinoProdottiCinesi
     {
         static void Main(string[] args)
         {
+            string scelta = "";
+            Magazzino magazzino = new Magazzino();
+            
+            while (scelta != "F")
+            {
+                Console.Clear();
+                Console.WriteLine("Cosa vuoi fare?");
+                Console.WriteLine("A) Vedere i prodotti in giacenza");
+                Console.WriteLine("B) Inserire un prodotto");
+                Console.WriteLine("C) Inserire uno stock prodotti");
+                Console.WriteLine("D) Eliminare un prodotto in giacenza");
+                Console.WriteLine("E) Cosa fai se sei arrabbiato?");
+                Console.WriteLine("F) Uscire");
+
+                scelta = Console.ReadLine();
+                if (scelta == "A")
+                {
+                    magazzino.VisualizzaProdotto();
+                }
+                else if (scelta == "B")
+                {
+                    magazzino.AggiungiProdotto();
+                }
+                else if (scelta == "C")
+                {
+                    magazzino.AggiungiStock();
+                }
+                else if (scelta == "D")
+                {
+                    magazzino.EliminaProdotto();
+                }
+                else if (scelta == "E")
+                {
+                    {
+                        bool haElementi = magazzino.prodottoInserito.Any();
+                        if (haElementi)
+                        {
+                            for (int i = 0; i < magazzino.prodottoInserito.Count; i++)
+                            {
+                                magazzino.prodottoInserito[i].Prodotto.SpaccaProdotto(magazzino);
+                            }
+                            Console.ReadKey();
+
+                        }
+                        else
+                        {
+                            Console.WriteLine("Non ci sono prodotti da spaccare");
+                            Console.ReadKey();
+                        }
+
+                    }
+
+
+
+                }
+
+            }
+
 
 
 
